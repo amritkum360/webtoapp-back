f7af4e69b0bf13e6a277eac125a7bcdbf853bf4c
Amrit Kumar <136947870+amritkum360@users.noreply.github.com>
2024-04-23 08:40:05 +0530

    Update dbconnect.js

--- modules/Models/dbconnect/dbconnect.js
+++ modules/Models/dbconnect/dbconnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/webtoapp').then(() => {
+mongoose.connect('mongodb+srv://Amritkum360:7004343011@cluster0.1bafcyc.mongodb.net/webtoapp').then(() => {
     console.log('Database connected successfully');
 }).catch((error) => {
     console.error('Error connecting to database:', error);
